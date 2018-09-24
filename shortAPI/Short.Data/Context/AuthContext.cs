65b6732cc61d26e51a827d3f308e13f13f5b63a4
smajko31 <smajko31@gmail.com>
2018-09-23 20:20:05 -0700

    add auth context class

---
+++ shortAPI/Short.Data/Context/AuthContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Short.Data.Context
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext")
+        {
+
+        }
+    }
+}
