dc865a72ff3a8c3f43181cc6335e860e654211f3
smajko31 <smajko31@gmail.com>
2018-09-23 19:55:39 -0700

    add startup class

---
+++ shortAPI/Short.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace Short.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
