54746f611789a04a258c098ed9b99cfbdefd091b
smajko31 <smajko31@gmail.com>
2018-09-23 22:32:56 -0700

    add provider

--- shortAPI/Short.API/Startup.cs
+++ shortAPI/Short.API/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using Short.Data.Providers;
+using System;
 using System.Web.Http;
 
 namespace Short.API
@@ -7,9 +11,26 @@ namespace Short.API
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthServiceProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
